28e3153b936492ceaa6403cd7ae1c4591fe9ca7f
ttinh12 <trantinh05062006@gmail.com>
2026-04-21 16:33:39 +0700

    [admin][fix lỗi vỡ layout]

--- Admin/View/dashboard.php
+++ Admin/View/dashboard.php
@@ -18,7 +18,7 @@
                     </div>
                     <div class="col-sm-5 text-center text-sm-left">
                         <div class="card-body pb-0 px-0 px-md-4">
-                            <img src="../assets/img/illustrations/man-with-laptop-light.png" height="140"
+                            <img src="Admin/assets/img/illustrations/man-with-laptop-light.png" height="140"
                                 alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png"
                                 data-app-light-img="illustrations/man-with-laptop-light.png" />
                         </div>
@@ -33,7 +33,7 @@
                         <div class="card-body">
                             <div class="card-title d-flex align-items-start justify-content-between">
                                 <div class="avatar flex-shrink-0">
-                                    <img src="../assets/img/icons/unicons/chart-success.png" alt="chart success"
+                                    <img src="Admin/assets/img/icons/unicons/chart-success.png" alt="chart success"
                                         class="rounded" />
                                 </div>
                                 <div class="dropdown">
@@ -58,7 +58,7 @@
                         <div class="card-body">
                             <div class="card-title d-flex align-items-start justify-content-between">
                                 <div class="avatar flex-shrink-0">
-                                    <img src="../assets/img/icons/unicons/wallet-info.png" alt="Credit Card"
+                                    <img src="Admin/assets/img/icons/unicons/wallet-info.png" alt="Credit Card"
                                         class="rounded" />
                                 </div>
                                 <div class="dropdown">
@@ -141,7 +141,7 @@
                         <div class="card-body">
                             <div class="card-title d-flex align-items-start justify-content-between">
                                 <div class="avatar flex-shrink-0">
-                                    <img src="../assets/img/icons/unicons/paypal.png" alt="Credit Card"
+                                    <img src="Admin/assets/img/icons/unicons/paypal.png" alt="Credit Card"
                                         class="rounded" />
                                 </div>
                                 <div class="dropdown">
@@ -166,7 +166,7 @@
                         <div class="card-body">
                             <div class="card-title d-flex align-items-start justify-content-between">
                                 <div class="avatar flex-shrink-0">
-                                    <img src="../assets/img/icons/unicons/cc-primary.png" alt="Credit Card"
+                                    <img src="Admin/assets/img/icons/unicons/cc-primary.png" alt="Credit Card"
                                         class="rounded" />
                                 </div>
                                 <div class="dropdown">
@@ -331,7 +331,7 @@
                         <div class="tab-pane fade show active" id="navs-tabs-line-card-income" role="tabpanel">
                             <div class="d-flex p-4 pt-3">
                                 <div class="avatar flex-shrink-0 me-3">
-                                    <img src="../assets/img/icons/unicons/wallet.png" alt="User" />
+                                    <img src="Admin/assets/img/icons/unicons/wallet.png" alt="User" />
                                 </div>
                                 <div>
                                     <small class="text-muted d-block">Total Balance</small>
@@ -382,7 +382,7 @@
                     <ul class="p-0 m-0">
                         <li class="d-flex mb-4 pb-1">
                             <div class="avatar flex-shrink-0 me-3">
-                                <img src="../assets/img/icons/unicons/paypal.png" alt="User" class="rounded" />
+                                <img src="Admin/assets/img/icons/unicons/paypal.png" alt="User" class="rounded" />
                             </div>
                             <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                 <div class="me-2">
@@ -397,7 +397,7 @@
                         </li>
                         <li class="d-flex mb-4 pb-1">
                             <div class="avatar flex-shrink-0 me-3">
-                                <img src="../assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
+                                <img src="Admin/assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
                             </div>
                             <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                 <div class="me-2">
@@ -412,7 +412,7 @@
                         </li>
                         <li class="d-flex mb-4 pb-1">
                             <div class="avatar flex-shrink-0 me-3">
-                                <img src="../assets/img/icons/unicons/chart.png" alt="User" class="rounded" />
+                                <img src="Admin/assets/img/icons/unicons/chart.png" alt="User" class="rounded" />
                             </div>
                             <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                 <div class="me-2">
@@ -427,7 +427,7 @@
                         </li>
                         <li class="d-flex mb-4 pb-1">
                             <div class="avatar flex-shrink-0 me-3">
-                                <img src="../assets/img/icons/unicons/cc-success.png" alt="User" class="rounded" />
+                                <img src="Admin/assets/img/icons/unicons/cc-success.png" alt="User" class="rounded" />
                             </div>
                             <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                 <div class="me-2">
@@ -442,7 +442,7 @@
                         </li>
                         <li class="d-flex mb-4 pb-1">
                             <div class="avatar flex-shrink-0 me-3">
-                                <img src="../assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
+                                <img src="Admin/assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
                             </div>
                             <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                 <div class="me-2">
@@ -457,7 +457,7 @@
                         </li>
                         <li class="d-flex">
                             <div class="avatar flex-shrink-0 me-3">
-                                <img src="../assets/img/icons/unicons/cc-warning.png" alt="User" class="rounded" />
+                                <img src="Admin/assets/img/icons/unicons/cc-warning.png" alt="User" class="rounded" />
                             </div>
                             <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                 <div class="me-2">
--- Admin/View/layouts/footer.php
+++ Admin/View/layouts/footer.php
@@ -1,35 +1,23 @@
-            <!-- Footer -->
-
+<!-- Footer -->
             <div class="content-backdrop fade"></div>
           </div>
-          <!-- Content wrapper -->
+          <!-- / Content wrapper -->
         </div>
         <!-- / Layout page -->
       </div>
+      <!-- / Layout container -->
 
       <!-- Overlay -->
       <div class="layout-overlay layout-menu-toggle"></div>
     </div>
     <!-- / Layout wrapper -->
 
-    <!-- <div class="buy-now">
-      <a
-        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
-        target="_blank"
-        class="btn btn-danger btn-buy-now"
-        >Upgrade to Pro</a
-      >
-    </div> -->
-
     <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
     <script src="Admin/assets/vendor/libs/jquery/jquery.js"></script>
     <script src="Admin/assets/vendor/libs/popper/popper.js"></script>
     <script src="Admin/assets/vendor/js/bootstrap.js"></script>
     <script src="Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-
     <script src="Admin/assets/vendor/js/menu.js"></script>
-    <!-- endbuild -->
 
     <!-- Vendors JS -->
     <script src="Admin/assets/vendor/libs/apex-charts/apexcharts.js"></script>
@@ -40,5 +28,5 @@
     <!-- Page JS -->
     <script src="Admin/assets/js/dashboards-analytics.js"></script>
 
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
+</html>
